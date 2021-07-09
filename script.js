daa1c4b99c4a3ca3549791f29f2d8bd6137f24e2
Reem122 <73790359+Reem122@users.noreply.github.com>
2021-07-09 13:37:48 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,17 +1,21 @@
 window.onscroll = function(){
     if(window.scrollY < 140){
-        document.getElementById('nav').classList.add('transparent-nav');
-        document.getElementById('nav').classList.remove('beige-nav');
-        document.getElementById('nav').classList.remove('black-nav');
+        document.getElementById('header').classList.add('transparent-nav');
+        document.getElementById('header').classList.remove('beige-nav');
+        document.getElementById('header').classList.remove('black-nav');
     }
     if(window.scrollY >= 140){
-        document.getElementById('nav').classList.add('beige-nav');
-        document.getElementById('nav').classList.remove('transparent-nav');
-        document.getElementById('nav').classList.remove('black-nav');
+        document.getElementById('header').classList.add('beige-nav');
+        document.getElementById('header').classList.remove('transparent-nav');
+        document.getElementById('header').classList.remove('black-nav');
     }
     if(window.scrollY >= 550){
-        document.getElementById('nav').classList.add('black-nav');
-        document.getElementById('nav').classList.remove('transparent-nav');
-        document.getElementById('nav').classList.remove('beige-nav');
+        document.getElementById('header').classList.add('black-nav');
+        document.getElementById('header').classList.remove('transparent-nav');
+        document.getElementById('header').classList.remove('beige-nav');
     }
+}
+function search(){
+    document.getElementById('page').classList.toggle('disappear');
+    document.getElementById('searching').classList.toggle('d-flex');
 }
